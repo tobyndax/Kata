b479811a81fb7e4c0179c270a8e741f9d80c0404
Jens Edhammer <tobyndax@hotmail.com>
2014-08-06 08:09:15 +0200

    Added a bunch of IOtests, and some crypto tests.

--- Crypto/crypto2.py
+++ Crypto/crypto2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import codecs
 import os
-import json
+import pickle
 
 def openFile(path):
     if(os.path.isfile(path)):
@@ -12,12 +13,95 @@ def openFile(path):
 def createFile(path):
     return codecs.open(path,encoding='utf-8',mode='w+')
 
-def getData(inFile):
+def getData(path):
+    inFile = openFile(path)
     data = inFile.read()
     return data
     
-def buildMethod():    
+def saveData(data, path):
+    file = createFile(path)
+    file.write(data)
+    file.close()
+    
+def plain(data):
+    return data 
+    
+def getInput(msg):
+    return raw_input(msg)
+    
+def getShift():
+    for i in range(3):
+        try:
+            shift = int(getInput("Please specify a shift (integer) for the ceasar chipher:"))
+            return shift
+        except TypeError:
+            print "Value must be of integer type"
+            pass
+    raise TypeError("Non Integer Value from user.")
+
+def buildShiftDict(shift):
+    alphabet=["a","b","c","d","e","f","g","h","i","j","k","l",  
+        "m","n","o","p","q","r","s","t","u","v","w","x","y","z",u"å",u"ä",u"ö"]
+    dic = {}
+    for i in range(len(alphabet)):
+        dic[alphabet[i]] = alphabet[(i+shift)%len(alphabet)]
+    return dic
+
+
+def ceasar(path,shift = 0):
+    #shift = 3
+    if(shift == 0):
+        print "Yes!!"
+        shift = getShift()
+    dataOut=""
+    inData = getData(path)
+    dic = buildShiftDict(shift)
+    for char in inData.lower():
+        if char in dic:
+            char = dic[char]
+        dataOut += char
+
+    return dataOut
+    
+def buildLibrary(): 
+    cryptoDict = dict()
+    cryptoDict['ceasar'] = ceasar
+    cryptoDict['plain'] = plain   
+    with open('library.dat', 'w') as outfile:
+      pickle.dump(cryptoDict, outfile)
+    
+def getLibrary():
+    with open('library.dat', 'r') as infile:
+      return pickle.load(infile)  
+
+def printMethods():
+    print "Available methods are:"
+    for key, value in getLibrary().iteritems():
+        print key
+         
+def getMethod(method):
+    try:
+        outMethod = getLibrary()[method]
+        return outMethod
+    except KeyError:
+        printMethods()
+        raise
+        
+def main():
+    buildLibrary()
+    
+    path = raw_input("Please specify the path to the textfile: ")
+    data = getData(path)
+    printMethods()
+    method = raw_input("Please choose a method to apply: ")
+    
+    
+    newData = getMethod(method)(path)
+    
+    newfilename = method+path
+    saveData(newData,newfilename)
+if __name__ == '__main__':    
+    main()
     
     
     
-def getMethod():
--- Crypto/testCrypto.py
+++ Crypto/testCrypto.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python 
+# -*- coding: utf-8 -*-
 from crypto2 import * 
 import unittest
 import os
+
+
+def touch(path):
+    open(path, 'a').close()
 
 class MyTest(unittest.TestCase):
     
     #File I/Otesting
     def test_open_file(self):
-        os.system("touch exist.txt")
+        touch('exist.txt')
         f = openFile('exist.txt')
         self.assertIsNotNone(f)
         f.close()
-        os.system("rm exist.txt")
+        os.remove('exist.txt')
     
     def test_open_file_noexist(self):
         self.assertRaises(IOError,openFile,'noexist.txt')
@@ -19,28 +24,52 @@ class MyTest(unittest.TestCase):
     def test_create_file_noexist(self):
         f = createFile('shouldexist.txt')
         self.assertIsNotNone(f)
-        os.system("rm shouldexist.txt")
+        os.remove('shouldexist.txt')
         
     def test_create_file_exist(self): 
-        os.system("touch exist.txt")
+        touch('exist.txt')
         f = createFile('exist.txt')
         self.assertIsNotNone(f)
         
-    def test_get_data_from_file(self):
+    def test_save_and_get_data_from_file(self):
         #---------Setup for file----------
-        a = u"this is a teststring"
-        f = createFile('test.txt')
-        f.write(a)
-        f.close()
-        #---------------------------------
-        f = openFile('test.txt')
-        self.assertEqual(getData(f),a,"Written data is not same as read data \n Original: %s \n File: %s"%(a,getData(f)))
+        a = "this is a teststring"
+        saveData(a,'test.txt')
+        self.assertEqual(getData('test.txt'),a,"Written data is not same as read data \n Original: %s \n File: %s"%(a,getData('test.txt')))
         
-        os.system("rm test.txt")
+        os.remove('test.txt')
         
+    def test_build_library(self):
+        touch('library.dat')
+        os.remove('library.dat')
+        buildLibrary()
+        self.assertIs(getLibrary()['plain'],plain,"The plain method is missing from library")
+        os.remove('library.dat')
         
-    def test_get_method_from_library(self):
+    def test_get_method(self):
+        buildLibrary()
+        self.assertIs(getMethod('plain'),plain,"Get method does not get 'plain'")
         
+    def test_get_method_noexist(self):
+        buildLibrary()
+        self.assertRaises(KeyError,getMethod,'garbage')
+        
+    def test_apply_method(self):
+        a = "this is a teststring"
+        data = getMethod('plain')(a)
+        self.assertEqual(data,a,"plain method not returning same string")
+    
+    def test_build_shift_dict(self):
+        a  = {u'\xf6': 'c', u'\xe5': 'a', u'\xe4': 'b', 'a': 'd', 'c': 'f', 'b': 'e', 'e': 'h', 'd': 'g', 'g': 'j', 'f': 'i', 'i': 'l', 'h': 'k', 'k': 'n', 'j': 'm', 'm': 'p', 'l': 'o', 'o': 'r', 'n': 'q', 'q': 't', 'p': 's', 's': 'v', 'r': 'u', 'u': 'x', 't': 'w', 'w': 'z', 'v': 'y', 'y': u'\xe4', 'x': u'\xe5', 'z': u'\xf6'}
+        self.assertEqual(a,buildShiftDict(3),"Build Shift does not shift correctly: \n %s"%buildShiftDict(3))
+    def test_Shift(self):
+        touch('test.txt')
+        file = codecs.open('test.txt',encoding='utf-8',mode='w+')
+        file.write(u"abcdefghijklmnopqrstuvwxyzåäö")
+        file.close()
+        b = u"defghijklmnopqrstuvwxyzåäöabc"
+        self.assertEqual(ceasar('test.txt',shift = 3),b,"Ceasar function not returning properly shifted data: %s"%ceasar('test.txt',3))
+
     
 if __name__ == '__main__':
     unittest.main()
